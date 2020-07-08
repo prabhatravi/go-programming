b8fe29ea61cf5c74776a38bbf396a1c3a3d85ceb
prabhatravi <prabhat.ravi.engg@gmail.com>
2020-07-08 21:11:20 +0530

    you are going good

--- 00_temp/variable.go
+++ 00_temp/variable.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
+var a int = 5
+var b string = "Prabhat Ravi"
+var c bool = true
+type d int
+var e d
 func main() {
-	$END$
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+	fmt.Println("c", c)
+	e = 9
+	a = int(e)
+	fmt.Println(a)
 }
