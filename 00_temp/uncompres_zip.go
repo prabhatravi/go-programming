81e83ef9f2cc46c58799e3edb2a815b9c2116409
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2020-07-25 08:45:31 +0530

    uncompress zip file

---
+++ 00_temp/uncompres_zip.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Please specify the techdump file.")
+		return
+	}
+	techDumpFile := os.Args[1]
+	zipReader, _ := zip.OpenReader(techDumpFile)
+	for _, file := range zipReader.Reader.File {
+		zippedFile, err := file.Open()
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer zippedFile.Close()
+
+		targetDir := "./"
+		extractedFilePath := filepath.Join(
+			targetDir,
+			file.Name,
+		)
+
+		if file.FileInfo().IsDir() {
+			log.Println("Directory Created:", extractedFilePath)
+			os.MkdirAll(extractedFilePath, file.Mode())
+		} else {
+			log.Println("File extracted:", file.Name)
+
+			outputFile, err := os.OpenFile(
+				extractedFilePath,
+				os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+				file.Mode(),
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer outputFile.Close()
+
+			_, err = io.Copy(outputFile, zippedFile)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
