f6f0ae81b12017c665e13a3dd8a0848e6759384f
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2020-09-03 23:11:48 +0530

    nested json

--- 00_temp/json_marshal.go
+++ 00_temp/json_marshal.go
@@ -7,18 +7,26 @@ import (
 
 type Person struct {
 	Name  string `json:"name"`
-	Profession string `json:"profession"`
+	Profession Profession `json:"profession"`
+}
+
+type Profession struct {
+	Hobby     string  `json:"hobby"`
+	Age       int  `json:"age"`
+	Developer bool `json:"is_developer"`
 }
 
 func main() {
 	// an instance
-	person := Person{Name: "Prabhat Ravi", Profession: "Software Engineer"}
+	profession := Profession{Hobby: "travelling", Age: 25, Developer: true}
+	person := Person{Name: "Prabhat Ravi", Profession: profession}
 	fmt.Println("person: ", person)
 
 	// To print key:value use %+v
 	fmt.Printf("person: %+v\n", person)
 
-	byteArray, err := json.Marshal(person)
+	//byteArray, err := json.Marshal(person)
+	byteArray, err := json.MarshalIndent(person, "", "  ")
 	if err != nil {
 		fmt.Println(err)
 	}
