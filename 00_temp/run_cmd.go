b61041ab5d53a094d253944239b70207ddd9d275
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2020-09-10 22:49:21 +0530

    run command and capture output

---
+++ 00_temp/run_cmd.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+)
+
+func main() {
+	cmd := exec.Command("ls", "-lah")
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("tasklist")
+	}
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("cmd.Run() failed with %s\n", err)
+	}
+	fmt.Printf("combined out:\n%s\n", string(out))
+}
