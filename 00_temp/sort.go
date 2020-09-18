c48afd367b38d03679875d011cf3982ca61e1f90
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2020-09-18 11:57:06 +0530

    sort

--- 00_temp/sort.go
+++ 00_temp/sort.go
@@ -11,6 +11,10 @@ func main() {
 	sort.Strings(strs)
 	fmt.Println("Strings:", strs)
 
+	strs1 := []string{"pk.0", "pk.1", "pk.11", "pk.2"}
+	sort.Strings(strs1)
+	fmt.Println("Strings:", strs1)
+
 	ints := []int{70, 2, 71}
 	sort.Ints(ints)
 	fmt.Println("Ints:   ", ints)
