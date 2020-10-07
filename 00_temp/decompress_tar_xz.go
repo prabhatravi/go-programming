2cb4e90177a330ec524faf08ebd6a6f8d9c8edb8
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2020-10-07 22:51:22 +0530

    corrected the path while extracting the tar

--- 00_temp/decompress_tar_xz.go
+++ 00_temp/decompress_tar_xz.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"archive/tar"
-	"fmt"
-	"io"
 	"log"
 	"os"
-
-	"github.com/xi2/xz"
 )
 
-func main() {
-	// Open a file
-	f, err := os.Open("myfile.tar.xz")
+//UntarTarXZ will extract xz compressed files to xpath directory
+func UntarTarXZ(tarFileName, xpath string) (err error) {
+	f, err := os.Open(tarFileName)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Could not open tarFileName: ", tarFileName)
+		return err
 	}
-	// Create an xz Reader
+	defer f.Close()
+	absPath, err := filepath.Abs(xpath)
+	if err != nil {
+		log.Println("Could not find Absolute path for: ", xpath)
+		return err
+	}
 	r, err := xz.NewReader(f, 0)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("XZ New reader failed for: ", tarFileName, "Error: ", err)
+		return err
 	}
 	// Create a tar Reader
 	tr := tar.NewReader(r)
@@ -31,29 +33,43 @@ func main() {
 			break
 		}
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error in getting the next tar. Error: ", err)
+			return err
+		}
+		finfo := hdr.FileInfo()
+		fileName := hdr.Name
+		absFileName := filepath.Join(absPath, fileName)
+		dir := filepath.Dir(absFileName)
+
+		if _, err := os.Stat(dir); err != nil {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				log.Println("Cannot create Dir: ", dir)
+				return err
+			}
 		}
 		switch hdr.Typeflag {
 		case tar.TypeDir:
 			// create a directory
-			fmt.Println("creating:   " + hdr.Name)
 			err = os.MkdirAll(hdr.Name, 0777)
 			if err != nil {
-				log.Fatal(err)
+				log.Println("Could not create dir: ", hdr.Name, "Error: ", err)
+				return err
 			}
 		case tar.TypeReg, tar.TypeRegA:
 			// write a file
-			fmt.Println("extracting: " + hdr.Name)
-			w, err := os.Create(hdr.Name)
+			w, err := os.OpenFile(absFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, finfo.Mode().Perm())
 			if err != nil {
-				log.Fatal(err)
+				log.Println("Error in creating file: ", absFileName, "Error: ", err)
+				return err
 			}
 			_, err = io.Copy(w, tr)
 			if err != nil {
-				log.Fatal(err)
+				log.Println("Error in writing file: ", absFileName, "Error: ", err)
+				return err
 			}
 			w.Close()
 		}
 	}
-	f.Close()
+
+	return nil
 }
